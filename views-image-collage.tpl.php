cace3b89020a7e3d840426a383d50c74aeb11ee3
Ciaran Phillips <ciaranphillips1@gmail.com>
2014-01-11 21:22:33 +0000

    Basic markup for list and overlays added to template

--- views-image-collage.tpl.php
+++ views-image-collage.tpl.php
@@ -7,6 +7,25 @@
  * @ingroup views_templates
  */
 ?>
-<?php //print render($image_collage); ?>
-yo
-<?php print_r($myvars) ?>
+<div class="image-collage-grid">
+    <ul>
+<?php foreach($blocks as $id => $b): ?>
+    
+        <li class="img-block" data-id="<?php echo $id ?>">    
+            <?php echo $b['img']; ?>
+            <div class="quote">
+                <?php echo $b['quote']; ?>
+            </div>
+        </li>
+        
+    
+<? endforeach; ?>
+    </ul>
+    <div id="image-collage-overlays">
+        <?php foreach($blocks as $id => $b): ?>
+            <div class="overlay" id="overlay-<?php echo $id ?>">
+                <?php echo $b['bio']; ?>
+            </div>
+        <? endforeach; ?>
+    </div>
+</div>
