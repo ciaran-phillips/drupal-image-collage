856e5dc6695a7146450729e4e30073c03478921f
Ciaran Phillips <ciaranphillips1@gmail.com>
2014-02-02 21:23:38 +0000

    quote overlays size and position respond to slider width change

--- image_collage.js
+++ image_collage.js
@@ -1,13 +1,14 @@
 (function ($) {
   
-  var row_width = 1000;
   var num_in_row = 6;
   var num_rows = 2;
   var margin = 80;
-  var img_width = row_width / num_in_row;
-  var img_height = img_width;
+  var img_width = 166;
+  var img_height = 166;
   var bg_height = (img_height * num_rows) + (margin  * 2) + "px";
-  
+  var overlay_span = 3;
+  var switch_point = 3;
+  var start_col = 0;
   
   
   $(document).ready(function() {
@@ -17,16 +18,16 @@
         $(this).css("height", img_height + "px");
       });
     $(".image-collage-close-button").css("margin-top", "-" + margin + "px");
-    $(".image-collage-grid").css("width", row_width + "px")
-    $(".image-collage-overlays").children().css("width", (img_width * num_in_row) + "px");
-    $(".quote, .quote-bg").css("width", (img_width * 3) + "px");
     $(".quote, .quote-bg").css("height", img_height + "px");
     $(".image-collage-grid li img").click(function() {
         open_overlay(
           $("#overlay-"+ $(this).parent().data("id"))
         );
       });
-  
+    update_overlay_width();
+    $(window).resize(function() {
+      update_overlay_width();
+    });
     $(".image-collage-close-button").click(function() {
       
       $(this).parent().parent().css("display","none");
@@ -39,6 +40,19 @@
     initialise_slider();
   });
   
+  function update_overlay_width() {
+    var total_width = $(".image-collage-grid").width();
+    if (total_width > ((img_width * 6) - 40)) {
+      overlay_span = 3;
+      num_in_row = 6;
+    }
+    else {
+      overlay_span = 2;
+      num_in_row = 4;
+    }
+    $(".quote, .quote-bg").css("width", (img_width * overlay_span) + "px");
+    update_cols(start_col);
+  }
   
   function update_cols(start_col) {
     $(".image-collage-grid .img-block").children(".quote, .quote-bg").each(function(){
@@ -55,7 +69,7 @@
       $(quote).css("left", img_width + "px");
     }
     else {
-      $(quote).css("left", "-" + (img_width * 3) + "px");
+      $(quote).css("left", "-" + (img_width * overlay_span) + "px");
     }
     if (row > 0) {
       $(quote).css("top", img_height + "px");
@@ -78,9 +92,9 @@
     };
     $(window).load(function() {
       $(".flexslider").flexslider({
-        itemWidth: 200,
+        itemWidth: 167,
         itemMargin:0,
-        minItems: 6,
+        minItems: 2,
         maxItems: 12,
         move: 1,
         slideshow:false,
@@ -88,7 +102,8 @@
         animationLoop:false,
         controlNav: false,
         after: function(slider){
-          update_cols(slider.currentSlide); 
+          update_cols(slider.currentSlide);
+          start_col = slider.currentSlide;
         }
       });     
     });
